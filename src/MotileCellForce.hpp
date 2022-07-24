21de277dbdfba2a4e629ffe73063bfdc43eda832
Tom Rusch <trusch@DESKTOP-019KEUV.localdomain>
2022-07-24 14:34:39 -0400

    Added Data Scripts, Updated Simulations

--- src/MotileCellForce.hpp
+++ src/MotileCellForce.hpp
@@ -1,4 +1,6 @@
-"""Copyright (c) 2005-2021, University of Oxford.
+/*
+
+Copyright (c) 2005-2015, University of Oxford.
 All rights reserved.
 
 University of Oxford means the Chancellor, Masters and Scholars of the
@@ -28,27 +30,67 @@ GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
 HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
 LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
 
-# Example SCons build script for user projects.
+*/
 
-import os
+/**********************************************
+ * THIS CODE WORKS WITH RELEASE 3.3 OF CHASTE *
+ **********************************************/
 
-# Get variables exported from the SConstruct file
-Import("*")
+#ifndef MOTILECELLFORCE_HPP_
+#define MOTILECELLFORCE_HPP_
 
-# Get our project directory name.
-# This assumes that this project is located at <chasteRoot>/projects/<project>.
-# Other things will probably go wrong if this is not the case...
-project_name = os.path.basename(os.path.dirname(os.path.dirname(os.getcwd())))
+#include "ChasteSerialization.hpp"
+#include <boost/serialization/base_object.hpp>
 
-# Chaste libraries used by this project.
-# Select which line to uncomment based on what your project needs, or alter as required.
-chaste_libs_used = ['core']
-#chaste_libs_used = ['cell_based']
-#chaste_libs_used = ['heart']
-#chaste_libs_used = ['cell_based', 'heart']
+#include "AbstractForce.hpp"
 
-# Do the build magic
-result = SConsTools.DoProjectSConscript(project_name, chaste_libs_used, globals())
-Return("result")
+// Needed here to avoid serialization errors (on Boost<1.37)
+#include "CellLabel.hpp"
+
+/**
+ * A motile cell force class.
+ */
+template<unsigned DIM>
+class MotileCellForce : public AbstractForce<DIM>
+{
+private:
+
+    friend class boost::serialization::access;
+    template<class Archive>
+    void serialize(Archive & archive, const unsigned int version)
+    {
+        archive & boost::serialization::base_object<AbstractForce<DIM> >(*this);
+    }
+
+public:
+
+    /**
+     * Constructor.
+     */
+    MotileCellForce();
+
+    /**
+     * Destructor.
+     */
+    ~MotileCellForce();
+
+    /**
+     * Overridden AddForceContribution() method.
+     *
+     * @param rCellPopulation a cell population object
+     */
+    void AddForceContribution(AbstractCellPopulation<DIM>& rCellPopulation);
+
+    /**
+     * Overridden OutputForceParameters() method.
+     *
+     * @param rParamsFile the file stream to which the parameters are output
+     */
+    void OutputForceParameters(out_stream& rParamsFile);
+};
+
+#include "SerializationExportWrapper.hpp"
+EXPORT_TEMPLATE_CLASS_SAME_DIMS(MotileCellForce)
+
+#endif /*MOTILECELLFORCE_HPP_*/
